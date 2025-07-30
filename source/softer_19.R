3e13df1dbb690395c3fd8af2d872a8a3eef5a104
Danielle Navarro <djnavarro@protonmail.com>
2025-07-31 09:54:57 +1000

    adds v17-v19

---
+++ source/softer_19.R
@@ -0,0 +1,117 @@
+seeds <- 1900 + (0:99)
+
+make_art <- function(seed) {
+    
+  sys_id <- "19"
+  sys_name <- "softer"
+  cpp_id <- "06"
+  cpp_path <- here::here("source", paste0(sys_name, "_", cpp_id, ".cpp"))
+  raster_data <- NULL # hack to silence linter
+  Rcpp::sourceCpp(cpp_path) # supplies render_data()
+  
+  # seed
+  cat(seed, "\n")
+  set.seed(seed)
+  
+  # fixed / default
+  px <- 2000
+  layers <- 50
+  million <- 10^6
+  iter <- 1000 * million
+  zoom <- .175
+  alpha <- .00001
+  
+  # palette specification ---------------------------------------------------
+  
+  palettes <- c(
+    "palette_01.csv",
+    "palette_02.csv",
+    "palette_03.csv",
+    "palette_04.csv",
+    "palette_05.csv"
+  ) |>
+    purrr::map(
+      \(x) here::here("source", "palettes", x) |>
+        readr::read_csv(show_col_types = FALSE)
+    ) |>
+    dplyr::bind_rows()
+
+  ncl <- 10
+  ind <- sample(nrow(palettes), 2)
+  palette_base <- unlist(palettes[ind,])
+  pal <- (colorRampPalette(palette_base))(ncl)
+  bg <- pal[1]
+  
+  
+  # helper functions --------------------------------------------------------
+  
+  generate_data <- function(seed, iter, layers, px, zoom, alpha) {
+    set.seed(seed)
+    df <- raster_data(iter, layers, px, zoom, alpha)
+    return(df)
+  }
+  
+  transform_data <- function(df) {
+    n_neg <- mean(df < 0)
+    df[df == 0] <- NA
+    df <- rank(
+      df, 
+      na.last = "keep", 
+      ties.method = "random"
+    )
+    df[is.na(df)] <- n_neg
+    df <- df - min(df)
+    df <- df / max(df)
+    df <- as.integer(df * (ncl - 1)) + 1
+    return(df)
+  }
+  
+  colourise_data <- function(df) {
+    df <- pal[df]
+    df <- matrix(df, px, px, byrow = TRUE)
+    return(df)
+  }
+  
+  render_data <- function(df, fpath, px, bg) {
+    rs <- as.raster(df)
+    jpeg(
+      filename = fpath,
+      width = px,
+      height = px,
+      bg = bg 
+    )
+    op <- par(mar = c(0,0,0,0))
+    plot(rs)
+    dev.off()
+    par(op)
+  }
+  
+  fpath <- function(seed) {
+    dir <- paste0("sys_", sys_id)
+    dir <- here::here("output", dir)
+    if(!dir.exists(dir)) dir.create(dir)
+    prefix <- paste0(sys_name, "_", sys_id, "_")
+    fname <- paste0(prefix, seed, ".jpg")
+    fp <- file.path(dir, fname)
+    return(fp)
+  }
+  
+  # generate the data -------------------------------------------------------
+  
+  cat("generating...\n")
+  
+  
+  df1 <- generate_data(seed, iter, layers, px, zoom, alpha)
+  
+  cat("transforming...\n")
+  
+  rank1 <- transform_data(df1)
+  cols1 <- colourise_data(rank1)
+  
+  cat("rendering...\n")
+  
+  render_data(cols1, fpath(seed), px, bg)
+  
+}
+
+for(s in seeds) make_art(s)
